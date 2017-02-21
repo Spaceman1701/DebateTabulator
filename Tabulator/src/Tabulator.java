081d8c41f9a68dcf08bcbeba992641861cc6d2f7
Spaceman1701 <fc.spaceman@gmail.com>
2017-02-20 18:43:13 -0700

    refactor project

---
+++ Tabulator/src/Tabulator.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Ethan on 2/20/2017.
+ */
+public class Tabulator {
+}
--- src/ethan/debate/Application.java
+++
@@ -1,7 +0,0 @@
-package ethan.debate;
-
-/**
- * Created by Ethan on 2/20/2017.
- */
-public class Application {
-}
