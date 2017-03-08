453a336a540f05664858cd222a8f144d11274680
Spaceman1701 <fc.spaceman@gmail.com>
2017-03-08 15:10:38 -0700

    switch to JAXB for xml parsing

--- Tabulator/src/ethan/tabulator/ruleset/RulesetLoader.java
+++ Tabulator/src/ethan/tabulator/ruleset/RulesetLoader.java
@@ -3,6 +3,8 @@ package ethan.tabulator.ruleset;
 import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 import com.sun.org.apache.xml.internal.serialize.OutputFormat;
 import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import ethan.tabulator.ruleset.jaxb.*;
+import ethan.tabulator.ruleset.jaxb.Ruleset;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -10,6 +12,8 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
@@ -22,7 +26,7 @@ public class RulesetLoader {
 
     private static final String RULSET_SCHEMA = "ruleset.xsd";
 
-    public RulesetLoader(String competitorTypesLocation, String eventTypesLocation, String rulesetLocation) throws InvalidXMLException {
+    public RulesetLoader(String competitorTypesLocation, String eventTypesLocation, String rulesetLocation) throws Exception {
         loadCompeitorTypes(competitorTypesLocation);
         loadEventTypes(eventTypesLocation);
 
@@ -37,16 +41,12 @@ public class RulesetLoader {
 
     }
 
-    private void loadRuleset(String location) throws InvalidXMLException {
-        Document ruleset = XMLUtils.loadXMLDocument(location, RULSET_SCHEMA);
-        Node root = ruleset.getDocumentElement();
-        System.out.println(root.getNodeName());
-        NodeList topNl = root.getChildNodes();
-        for (int i = 0; i < topNl.getLength(); i++) {
-            if (topNl.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                System.out.println(topNl.item(i).getNodeName());
-            }
-        }
+    private void loadRuleset(String location) throws Exception {
+        JAXBContext context = JAXBContext.newInstance("ethan.tabulator.ruleset.jaxb");
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        ethan.tabulator.ruleset.jaxb.Ruleset rs = (Ruleset)
+                unmarshaller.unmarshal(new File(XMLUtils.RESOURCE_DIR + location));
+        System.out.println(rs.getName());
     }
 
     /**
